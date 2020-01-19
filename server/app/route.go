2fd034a26f5ecad28704032dc91b9d43fb83b532
steven victor <chikodi543@gmail.com>
2020-01-20 00:37:47 +0100

    Remove redundant code

--- server/app/route.go
+++ server/app/route.go
@@ -1,12 +1,14 @@
 package app
 
 import (
+	"github.com/gin-gonic/contrib/static"
 	"username_across_platforms/server/controller"
 	"username_across_platforms/server/middleware"
 )
 
 func route() {
 	router.Use(middleware.CORSMiddleware()) //to enable api request between client and server
+	router.Use(static.Serve("/", static.LocalFile("./web", true))) //for the vue app
 
 	router.POST("/username", controller.Username)
 }
